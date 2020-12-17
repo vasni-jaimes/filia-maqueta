f108b035a13be489791263c81148cb7bace1e9af
Vasni Jaimes <vasnijaimes@Vasnis-MacBook-Pro.local>
2020-12-17 08:56:14 -0600

    maqueta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let slide = document.querySelector('.slide');
+let slide = document.querySelector('.slide-index');
 if (slide) {
 	$('.slide').slick({
 	  dots: false,
@@ -8,4 +8,33 @@ if (slide) {
 	  autoplay: true,
 	  cssEase: 'linear'
 	});
+}
+
+
+let menu = document.querySelector('.burger');
+if (menu) {
+	menu.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('nav').classList.add('active');
+	})
+}
+
+let close = document.querySelector('nav .close');
+if (close) {
+	close.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('nav').classList.remove('active');
+		document.querySelector('.contacto').style.display = "none";
+	})
+}
+
+
+//EFECTO DE ALICK AL MENU
+itemMenu = document.querySelectorAll('header nav a');
+if (itemMenu) {
+	for (var i = 0; i <= itemMenu.length - 1; i++) {
+		itemMenu[i].addEventListener('click', function(e) {
+			$('nav').removeClass();
+		});
+	}
 }
